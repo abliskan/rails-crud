f32be2c2f71cc15253354b56aa492f0d3225fd0b
RickyS <35142826+abliskan@users.noreply.github.com>
2022-04-19 22:09:14 +0700

    Update food_spec.rb

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,33 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  it "is invalid with a duplicate name" do
-    food = Food.create(
-      name: "Nasi Uduk",
-      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
-      price: 10000.0
-    )
-    
-    it 'is valid with a name and a description' do
-      food = Food.new(
-        name: 'Nasi Uduk',
-        description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
-        price: 15000.0
+  describe 'self#by_letter' do
+    it "should return a sorted array of results that match" do
+      food1 = Food.create(
+        name: "Nasi Uduk",
+        description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+        price: 10000.0
       )
-  
-      expect(food).to be_valid
+
+      food2 = Food.create(
+        name: "Kerak Telor",
+        description: "Betawi traditional spicy omelette made from glutinous rice cooked with egg and served with serundeng.",
+        price: 8000.0
+      )
+
+      food3 = Food.create(
+        name: "Nasi Semur Jengkol",
+        description: "Based on dongfruit, this menu promises a unique and delicious taste with a small hint of bitterness.",
+        price: 8000.0
+      )
+
+      expect(Food.by_letter("N")).to eq([food3, food1])
     end
-  
-    it 'is invalid without a name' do
-      food = Food.new(
-        name: nil,
-        description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
-        price: 15000.0
-      )
-  
-    food.valid
-  
-    expect(food.errors[:name]).to include("can't be blank")
-    
   end
 end
