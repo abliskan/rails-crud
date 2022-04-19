e784d42d51147020166992881bf55e1a757346c4
RickyS <35142826+abliskan@users.noreply.github.com>
2022-04-19 21:06:26 +0700

    food_spec description test

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid with a duplicate name" do
+    food1 = Food.create(
+      name: "Nasi Uduk",
+      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      price: 10000.0
+    )
+    
+    food2 = Food.new(
+      name: "Nasi Uduk",
+      description: "Just with a different description.",
+      price: 10000.0
+    )
+    food2.valid?
+    
+    expect(food2.errors[:name]).to include("has already been taken")
+    
+  end
 end
